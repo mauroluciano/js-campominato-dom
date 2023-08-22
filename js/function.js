ae2479ef91176177cef5556873f099f896d47e4b
mauroluciano <mauroluciano911@gmail.com>
2023-08-22 17:41:48 +0200

    add fix

--- js/function.js
+++ js/function.js
@@ -3,6 +3,19 @@ button.addEventListener("click", function () {
   const cellsContainer = document.getElementById("cells-container");
   const totalCell = 10 * 10;
 
+  // nella costante bomb c'è un array
+  const bomb = [];
+  // loop-controllo che la lunghezza di bomb sia inferiore a 16
+  while (bomb.length < 16) {
+    const random = getRandomNumber();
+
+    // controllo per evitare d'inserire due volte lo stesso numero
+
+    if (!bomb.includes(random)) {
+      bomb.push(random);
+    }
+  }
+
   //- creo una lista di numeri da 1 a 100
   for (let i = 1; i <= totalCell; i++) {
     const cell = document.createElement("li");
@@ -13,6 +26,17 @@ button.addEventListener("click", function () {
     //- creo una lista di numeri da 1 a 100
     cell.addEventListener("click", function () {
       cell.classList.add("yellow");
+
+      //- creo una lista di numeri da 1 a 100
+      if (bomb.includes(i)) {
+        cell.classList.add("red");
+      }
     });
   }
 });
+
+// generiamo un numero casuale da 1 a 100 con la funzione
+function getRandomNumber() {
+  const RandomNumber = Math.floor(Math.random() * 100) + 1;
+  return RandomNumber;
+}
