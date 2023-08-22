b7c6f3c9092bbb439883084d3e36a9a068312f64
mauroluciano <mauroluciano911@gmail.com>
2023-08-22 15:22:52 +0200

    add functions

---
+++ js/function.js
@@ -0,0 +1,18 @@
+let button = document.getElementById("generate-grid");
+button.addEventListener("click", function () {
+  const cellsContainer = document.getElementById("cells-container");
+  const totalCell = 10 * 10;
+
+  //- creo una lista di numeri da 1 a 100
+  for (let i = 1; i <= totalCell; i++) {
+    const cell = document.createElement("li");
+    cell.innerText = i;
+    cell.classList.add("cell");
+    cellsContainer.append(cell);
+
+    //- creo una lista di numeri da 1 a 100
+    cell.addEventListener("click", function () {
+      cell.classList.add("yellow");
+    });
+  }
+});
